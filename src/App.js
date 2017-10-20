bfe0585b35e8ad5e60859e04f27aa627ec5a9e00
NETLIGHT\temu <temu@netlight.com>
2017-10-20 14:27:07 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Menu from './components/Menu';
 import Content from './components/Content';
 import {Row, Col, Grid} from 'react-bootstrap';
+import {BrowserRouter} from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -10,14 +11,16 @@ class App extends Component {
       <div>
         <Header />
         <Grid>
-          <Row>
-            <Col sm={3}>
-              <Menu />
-            </Col>
-            <Col sm={7}>
-              <Content />
-            </Col>
-          </Row>
+          <BrowserRouter>
+            <Row>
+              <Col sm={3}>
+                <Menu />
+              </Col>
+              <Col sm={7}>
+                <Content />
+              </Col>
+            </Row>
+          </BrowserRouter>
         </Grid>
       </div>
     );
